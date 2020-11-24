89ae5575a5d6f69ce646e6493f6d454a7dfbf827
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2020-11-23 21:45:14 -0500

    my meds site initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Default</a>
+        </div>
+      </nav>
     </div>
   );
 }
