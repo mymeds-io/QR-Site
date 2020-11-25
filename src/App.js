3830cb41d7eb80cd90ed4c620acbd883fd1add66
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2020-11-24 21:08:43 -0500

    signIn container update 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import NavBarComponent from './components/NavBar/NavBarComponent';
+import SignInComponent from './components/SignIn/SignInComponent';
 
 function App() {
   return (
     <div className="App">
         <NavBarComponent />
-        <div className="signInContent">
-          <div className="row no-gutters align-items-center justify-content-center" style={{transform: "translateY(25%)"}}>
-            <div className="col-7 col-md-4 col-lg-3">
-              <div className="signInContainer">
-                
-              </div>
-            </div>
-          </div>
-        </div>
+        <SignInComponent />
     </div>
   );
 }
---
+++ src/components/SignIn/SignInComponent.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './signIn.css';
+
+export default function SignInComponent() {
+    return (
+        <div className="signInContent">
+            <div className="row no-gutters align-items-center justify-content-center" style={{transform: "translateY(25%)"}}>
+                <div className="col-7 col-md-4 col-lg-3">
+                    <div className="signInContainer">
+                        <div className="container-fluid" style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+                            <div className="row no-gutters" style={{width: "100%"}}>
+                                <h5 className="col-12" style={{textAlign: "center"}}>
+                                    All about your meds...
+                                </h5>
+                            </div>
+                            <div className="row no-gutters" style={{width: "100%"}}>
+                                <h6 className="col-12" style={{textAlign: "center"}}>
+                                    Sign in to your account
+                                </h6>
+                            </div>
+                            <div className="row no-gutters" style={{width: "100%"}}>
+                                <div className="col-12">
+                                    <form>
+                                        <div className="form-group">
+                                            <label for="exampleInputEmail1">Email address</label>
+                                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        </div>
+                                        <div className="form-group">
+                                            <label for="exampleInputPassword1">Password</label>
+                                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <button type="submit" className="signInSubmit btn btn-primary">Submit</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="row no-gutters" style={{width: "100%"}}>
+                                <div className="col-6">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
