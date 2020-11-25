55f81ea1b00d65a2a22fc157b0f0a901a2b1c588
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-25 16:09:57 -0500

    signUp Component 2 initial commit

--- src/components/SignUp/signUpComponent.js
+++ src/components/SignUp/signUpComponent.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './signUp.css';
+import myMedsLogo from '../../images/myMedsLogo.png';
+
 
 export default function SignUpComponent() {
     return (
-        <div>
-            
+        <div className="signUpComponent">
+            <div className="leftMyMedsSection">
+                <div className="row no-gutters justify-content-center" style={{width: "100%", position: "relative", bottom: "7vh"}}>
+                    <div className="col-6">
+                        <img src={myMedsLogo} alt="signUpMyMedsLogo" className="img-fluid"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
