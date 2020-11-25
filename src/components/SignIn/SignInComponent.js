327cfc62a4b82cf50ca28dbafc26db76df2fa8fb
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2020-11-24 23:16:45 -0500

    signIn container update 5

--- src/components/SignIn/SignInComponent.js
+++ src/components/SignIn/SignInComponent.js
@@ -5,14 +5,14 @@ export default function SignInComponent() {
     return (
         <div className="signInContent">
             <div className="row no-gutters align-items-center justify-content-center" style={{transform: "translateY(25%)"}}>
-                <div className="col-7 col-md-4 col-lg-3">
+                <div className="col-8 col-md-4 col-lg-3">
                     <div className="signInContainer">
-                        <div className="container-fluid" style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+                        <div className="container-fluid" style={{display: "flex", justifyContent: "space-around", alignItems: "center", flexDirection: "column", height: "100%"}}>
                             <div className="row no-gutters" style={{width: "100%"}}>
-                                <h5 className="col-12" style={{textAlign: "center"}}>
+                                <h5 className="signInHeader col-12" style={{textAlign: "center"}}>
                                     All about your meds...
                                 </h5>
-                                <h6 className="col-12" style={{textAlign: "center"}}>
+                                <h6 className="signInSubHeader col-12" style={{textAlign: "center"}}>
                                     Sign in to your account
                                 </h6>
                             </div>
@@ -20,10 +20,10 @@ export default function SignInComponent() {
                                 <div className="col-12">
                                     <form>
                                         <div className="form-group">
-                                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                                            <input type="email" className="form-control" style={{fontFamily: "FontAwesome"}} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="&#xf0e0;   Email"/>
                                         </div>
                                         <div className="form-group">
-                                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                                            <input type="password" className="form-control" style={{fontFamily: "FontAwesome"}} id="exampleInputPassword1" placeholder="&#xf023;    Password" />
                                             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                                         </div>
                                         <div className="row">
