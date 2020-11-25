10c08abc780deed68291c4b706c005f578e1bf7f
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-25 17:05:28 -0500

    signUp component 2 update 2

--- src/components/SignUp/signUpComponent.js
+++ src/components/SignUp/signUpComponent.js
@@ -6,13 +6,66 @@ import myMedsLogo from '../../images/myMedsLogo.png';
 export default function SignUpComponent() {
     return (
         <div className="signUpComponent">
-            <div className="leftMyMedsSection">
-                <div className="row no-gutters justify-content-center" style={{width: "100%", position: "relative", bottom: "7vh"}}>
+            <span className="leftMyMedsSection">
+                <div className="row no-gutters justify-content-center" style={{width: "100%", position: "relative", bottom: "8vh"}}>
                     <div className="col-6">
                         <img src={myMedsLogo} alt="signUpMyMedsLogo" className="img-fluid"/>
                     </div>
                 </div>
-            </div>
+            </span>
+            <span className="rightMyMedsSection">
+                <div className="row no-gutters justify-content-center" style={{width: "100%"}}>
+                    <div className="col-7" style={{position: "relative", right: "1vw"}}>
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4"/>
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputPassword4">Password</label>
+                                <input type="password" class="form-control" id="inputPassword4"/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress">Address</label>
+                                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputAddress2">Address 2</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                <label for="inputCity">City</label>
+                                <input type="text" class="form-control" id="inputCity"/>
+                                </div>
+                                <div class="form-group col-md-4">
+                                <label for="inputState">State</label>
+                                <select id="inputState" class="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                                </div>
+                                <div class="form-group col-md-2">
+                                <label for="inputZip">Zip</label>
+                                <input type="text" class="form-control" id="inputZip"/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck"/>
+                                <label class="form-check-label" for="gridCheck">
+                                    Check me out
+                                </label>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </form>
+                    </div>
+                </div>
+            </span>
         </div>
     )
 }
+
