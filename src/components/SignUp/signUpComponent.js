a2c7d5b48f99f6ced2bb72dce93bb95e4aca9963
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-26 15:07:02 -0500

    signUp Mobile Container

--- src/components/SignUp/signUpComponent.js
+++ src/components/SignUp/signUpComponent.js
@@ -58,6 +58,49 @@ export default function SignUpComponent() {
             </div>
             <div className="d-block d-sm-block d-md-none d-lg-none signUpComponentMobile">
                 <NavBarComponent />
+                <div className="signUpContentMobile">
+                    <div className="row no-gutters align-items-center justify-content-center" style={{transform: "translateY(20%)"}}>
+                        <div className="col-8">
+                            <div className="signUpContainerMobile">
+                                <form>
+                                    <div className="signUpInputsMobile">
+                                        <div className="form-row justify-content-center">
+                                            <div className="form-group col-5">
+                                            <input type="email" className="form-control" id="inputEmail4" placeholder="First Name" />
+                                            </div>
+                                            <div className="form-group col-5">
+                                            <input type="password" className="form-control" id="inputPassword4" placeholder="Last Name" />
+                                            </div>
+                                        </div>
+                                        <div className="form-row justify-content-center">
+                                            <div className="form-group col-11">
+                                                <input type="text" className="form-control" id="inputAddress" placeholder="Email Address"/>
+                                            </div>
+                                        </div>
+                                        <div className="form-row justify-content-center">
+                                            <div className="form-group col-11">
+                                                <input type="text" className="form-control" id="inputAddress2" placeholder="Create Password"/>
+                                            </div>
+                                        </div>
+                                        <div className="form-row justify-content-center">
+                                            <div className="form-group col-11">
+                                            <input type="text" className="form-control" id="inputCity" placeholder="Confirm Password" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row no-gutters justify-content-center" style={{width: "100%", position: "relative", top: "10vh"}}>
+                                        <div className="col-11">
+                                            <button type="submit" className="signUpBtn btn btn-primary" >Sign up for myMeds</button>
+                                        </div>
+                                        <div className="col-11" style={{textAlign: "center", position: "relative", top: "2vh"}}>
+                                            <small className="form-text text-muted">By clicking "Sign up for myMeds" You agree to our Terms of Service and Privacy Statement</small>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
