684d3252ce72d21f4f31438f39f9f48b83745556
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-30 02:52:53 -0500

    medlist component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import HomeComponent from './components/Home/HomeComponent';
 import SignUpComponent from './components/SignUp/SignUpComponent';
+import MedListComponent from './components/MedList/MedListComponent';
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomeComponent} />
           <Route exact path="/sign-up" component={SignUpComponent} />
+          <Route exact path="/user-meds" component={MedListComponent} />          
         </Switch>
       </div>
     )
--- src/components/MedList/MedListComponent.js
+++ src/components/MedList/MedListComponent.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import NavBarComponent from '../NavBar/NavBarComponent'
 
 export default function MedListComponent() {
     return (
-        <div>
-            
+        <div className="medListComponent">
+            <NavBarComponent />
+            <div className="medListContent">
+                <div className="row">
+                    <div className="col-10">
+                        <div className="medListContainer">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
