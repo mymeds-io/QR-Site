92215bcdf630eda2265d3e1ba1e25bf50943f012
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-30 04:19:52 -0500

    med container

--- src/components/MedList/MedListComponent.js
+++ src/components/MedList/MedListComponent.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import NavBarComponent from '../NavBar/NavBarComponent'
+import React from 'react';
+import NavBarComponent from '../NavBar/NavBarComponent';
+import './medList.css';
 
 export default function MedListComponent() {
     return (
         <div className="medListComponent">
             <NavBarComponent />
             <div className="medListContent">
-                <div className="row">
-                    <div className="col-10">
+                <div className="row no-gutters justify-content-center">
+                    <div className="col-9 col-md-4 col-lg-4">
                         <div className="medListContainer">
                             
                         </div>
