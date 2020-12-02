b72e469cfdd7831c20dfcb451920efcd73b71cad
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-12-01 20:19:50 -0500

    update 5, user pic included

--- src/components/MedList/MedListComponent.js
+++ src/components/MedList/MedListComponent.js
@@ -18,7 +18,7 @@ export default function MedListComponent() {
             return meds.map((med) => {
                 return (
                 <div className="row no-gutters medsRow justify-content-center mt-3">
-                    <div className="col-2" style={{position:"relative", right:"1vw"}}>
+                    <div className="col-1" style={{position:"relative", right:"1vw"}}>
                         <img className="img-fluid" src={med.image} alt="medPic"/>
                     </div>
                     <div className="col-8" style={{position:"relative", left:"1vw", top:"1vh"}}>
@@ -33,7 +33,7 @@ export default function MedListComponent() {
                                 <div className="medRefill" style={{position:"relative", right:"1vw"}}><i className="fa fa-share" style={{fontWeight:"700"}} aria-hidden="true">{med.refill}</i></div>
                             </div>
                             <div className="col-5" style={{display:"flex", justifyContent:"center"}}>
-                                <div className="medExp" style={{position:"relative", right:"3vw"}}><i class="fa fa-ban" style={{fontWeight:"700"}} aria-hidden="true">{med.exp}</i></div>
+                                <div className="medExp" style={{position:"relative", right:"10%"}}><i class="fa fa-ban" style={{fontWeight:"700"}} aria-hidden="true">{med.exp}</i></div>
                             </div>
                         </div>
                     </div>
@@ -50,7 +50,7 @@ export default function MedListComponent() {
 
     const renderAge = () => {
         if(age){
-            return <h6>{age + " Years Old"}</h6>
+            return <h6 className="userAge" >{age + " Years Old"}</h6>
         }
     }
 
@@ -74,31 +74,36 @@ export default function MedListComponent() {
                 <div className="row no-gutters justify-content-center" style={{transform: "translateY(15%)"}}>
                     <div className="col-9 col-md-4 col-lg-4">
                         <div className="medListContainer">
-                            <div className="row no-gutters">
-                                <div className="col-4">
-                                    {renderName()}
-                                </div>
-                            </div>
-                            <div className="row no-gutters">
-                                <div className="col-3">
-                                    {renderAge()}
-                                </div>
-                            </div>
-                            <div className="row no-gutters justify-content-center">
-                                <div className="col-5">
-                                    <div className="medHx medHistoryContainer active">
-                                        <div className="medHxOverlay"></div>
-                                        <span style={{textAlign:"center", fontSize:".9rem"}}>Medicine Tracking List</span>
+                            <img src="https://avatars2.githubusercontent.com/u/49351487?s=460&u=a49783af1f3ab38550aae809c9cc4ecdaa432c7c&v=4" alt="userPic" className="userImg img-fluid"/>    
+                            <div className="medListContainerContent">
+                                <div className="row no-gutters userNameRow">
+                                    <div className="col-4" style={{position:"relative", left:"5%"}}>
+                                        {renderName()}
                                     </div>
                                 </div>
-                                <div className="col-5">
-                                    <div className="medHx medHistoryContainer2">
-                                        <div className="medHxOverlay2"></div>
-                                        <span>History</span>
+                                <div className="row no-gutters">
+                                    <div className="col-3" style={{position:"relative", left:"6%"}}>
+                                        {renderAge()}
                                     </div>
                                 </div>
+                                <div className="row no-gutters justify-content-center">
+                                    <div className="col-5">
+                                        <div className="medHx medHistoryContainer active">
+                                            <div className="medHxOverlay"></div>
+                                            <span style={{textAlign:"center", fontSize:".9rem"}}>Medicine Tracking List</span>
+                                        </div>
+                                    </div>
+                                    <div className="col-5">
+                                        <div className="medHx medHistoryContainer2">
+                                            <div className="medHxOverlay2"></div>
+                                            <span>History</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="userMedContainer">
+                                    {renderMeds()}                                
+                                </div>
                             </div>
-                                {renderMeds()}                                
                         </div>
                     </div>
                 </div>
