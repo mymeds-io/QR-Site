211fe536ccf207fb766fc78dcdb4a4a3bffb6e29
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2021-01-18 18:15:49 -0500

    tracked pts component update

--- src/components/User/TrackedPtComponent.js
+++ src/components/User/TrackedPtComponent.js
@@ -6,17 +6,17 @@ function TrackedPtComponent(){
         <div className="row no-gutters justify-content-center" style={{width:"100%", height:"15%"}}>
             <div className="col-11" style={{height:"100%", backgroundColor:"blue"}}>
                 <div className="row no-gutters" style={{width:"100%", height:"100%"}}>
-                    <div className="col-2 ptImage d-flex" style={{height:"100%", backgroundColor:"red", alignItems:"center", justifyContent:"center"}}>
+                    <div className="col-2 ptImage d-flex" style={{height:"100%", backgroundColor:"red", alignItems:"center", justifyContent:"center", textAlign:'center'}}>
                         {"Add pt image"}
                     </div>
                     <div className="col-5 ptNameAndTime d-flex" style={{height:"100%", backgroundColor:"yellow", justifyContent:"center", flexDirection:"column"}}>
                         <div className="row no-gutters">
-                            <div className="col-12" style={{backgroundColor:"blue"}}>
+                            <div className="col-12" style={{backgroundColor:"blue", paddingLeft:'2vw'}}>
                                 {"Pt Name"}
                             </div>
                         </div>
                         <div className="row no-gutters">
-                            <div className="col-12" style={{backgroundColor:"blue"}}>
+                            <div className="col-12" style={{backgroundColor:"blue", paddingLeft:'2vw'}}>
                                 {"Last updated"}
                             </div>
                         </div>
