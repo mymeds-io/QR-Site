52e3e7f059f841e476ffa22029dba4ddeca86605
Gregmar Cruz D.O <gregr1201@gmail.com>
2021-02-21 01:18:06 -0500

    forgotPass component and functionality working
    Still need to fix url from test url (i.e. remove https:// from SendGrid reset url)
    
    And still need to implement reset password functionality

--- src/components/Forgot/ForgotPassComponent.js
+++ src/components/Forgot/ForgotPassComponent.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import NavBarComponent from '../NavBar/NavBarComponent';
 import './forgotPass.css';
+import TrueVaultClient from 'truevault';
 
 export default function ForgotPassComponent() {
 
     const [email, setEmail] = useState('')
+
+    const scopedAccessTokenAuth = 'c2F0LWU4YzVmZmU0LWNmZGEtNDE2NS05ZDg5LWZmMWE2MGIzMDMxMToyaUNuT1NnWjNGUERHU1h1NzAwT1U0OV9EVWMyckRvZHBSRkJSV3R6OHlN';
+    const passwordResetFlowId = 'fe06359a-a5f4-4ffc-b762-b0844756887c';
+    const tvClient = new TrueVaultClient({httpBasic: scopedAccessTokenAuth});
+
+    const submitResetLink = async (event, email) => {
+        event.preventDefault();
+
+        try {
+          await tvClient.sendPasswordResetEmail(passwordResetFlowId, email)
+          alert('Success! Please check your email for the reset link')  
+          setEmail('')
+        } catch (error) {
+            alert(`We're very sorry. We were unable to send you a reset password link. Please try again later.`)
+        }
+    }
 
     return (
         <div className="forgotPassComponent">
@@ -14,7 +31,7 @@ export default function ForgotPassComponent() {
                     <div className="row no-gutters" style={{width: '100%'}}>
                         <div className="col-12 inputEmailReset">
                             <input onChange={e => setEmail(e.target.value)} value={email} className="resetInput" type="text" placeholder="myMedsRec Email" />
-                            <button className="btn btn-primary submitResetBtn">Send reset link</button>
+                            <button onClick={(event) => submitResetLink(event, email)} className="btn btn-primary submitResetBtn">Send reset link</button>
                         </div>
                     </div>
                 </div>
