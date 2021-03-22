4e6e2c69f1bac9c6bc4009217c807605c48bace4
Gregmar Cruz D.O <gregr1201@gmail.com>
2021-03-21 21:19:45 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 const apiKey = '1dd1de89-9af6-462a-a701-27b99410e495';
-const accountId = 'b47d8b19-1301-4050-a69b-255faada6c2c';
+const accountId = '59b231e0-1d9d-4d0e-972f-46dbc605783d';
 const tvGroupDocId = `c53210fe-1d0b-4fa3-bf63-dc239a7e5285`;
 const mfaGroupId = '25b7fc62-ca9a-42b4-a269-5eed558c002e';
 const adminApiKey = `d1bc2972-1213-41d7-99b0-1546fd06d24f`;
